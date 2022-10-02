53aa35ef52f9a231f09d9be44e4b3966294465be
sb <sb@450G7>
2022-10-02 16:56:22 +0300

    ufak ilerlemeler

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 
 import React, {useState} from 'react';
-
+import AdminDiscountTable from "./components/AdminDiscountTable/AdminDiscountTable";
 function App() {
  
+
+  return (
+    <div>
+
+      <AdminDiscountTable baseUrl={""}></AdminDiscountTable>
+    </div>
+  );
 }
   
 export default App;
